71b0b5c48579007f32de855b180fbd5bc08381f4
Harold J. Giddings <harold@giddings.me>
2016-05-20 15:52:51 -0400

    initial commit
    initial commit

---
+++ FldigiController/Library.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CookComputing.XmlRpc;
+
+
+namespace FldigiController
+{
+    [XmlRpcUrl("http://localhost:7362")]   //address of the fldidi xmlrpc server
+    public interface Flrpc : IXmlRpcProxy
+    {
+        [XmlRpcMethod("fldigi.name")] //Returns name of the program
+        String FldigiName();
+
+        [XmlRpcMethod("fldigi.version")] //Returns version of the program
+        String FldigiVersion();
+
+        [XmlRpcMethod("modem.getname")] //Returns name of current modem
+        String ModemGetName();
+
+        [XmlRpcMethod("modem.set_by_name")] //Sets the modem by a name string. It returns the pervious modem.
+        String ModemSetbyName();
+
+        [XmlRpcMethod("modem.set_by_id")] //Sets modem by the ID.
+        int ModemSetbyId();
+
+        [XmlRpcMethod("text.clear_rx")] //Clear rx text window
+        String TextClearRx();
+
+        [XmlRpcMethod("main.tx")] //Sets mode to transmit
+        String MainTx();
+
+        [XmlRpcMethod("main.get_trx_status")] //Grabs the current rx/tx status
+        String MainGetTrxStatus();
+
+    }
+    public class flControl
+    {
+        public static void Connect()
+        {
+            Console.WriteLine("DEBUG: Called from FldigiController"); //Prints debug line to console
+            Flrpc proxy = XmlRpcProxyGen.Create<Flrpc>();
+//            Console.WriteLine("Current Status " + proxy.MainGetTrxStatus());
+//            Console.WriteLine("DEBUG: Clearing text" + proxy.TextClearRx());
+//            Console.WriteLine("Version " + proxy.FldigiVersion());
+//            Console.WriteLine("DEBUG: Beginning Transmit" + proxy.MainTx());
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibraryTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FldigiController;
+
+namespace LibraryTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            flControl.Connect();
+        }
+    }
+}
